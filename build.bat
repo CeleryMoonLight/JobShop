02c30bbdb1afc10461463b832a3cc096cefb9b34
MoonLightQ <superqjx@hotmail.com>
2018-05-01 10:53:49 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,15 @@
-@echo off
-cd build
-cmake ..
+@ECHO off
+ECHO Checking CMake...
+WHERE cmake >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO CMake wasn't found on your device.
+    ECHO Please go to https://cmake.org/ to install CMake. 
+    PAUSE
+) ELSE (
+    ECHO CMake was found.
+    IF NOT EXIST build (
+        MD build
+    )
+    CD build
+    cmake ..
+)
